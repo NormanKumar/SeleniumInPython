8e4f84faae9196da7ecbdf9723fc18aedefe38c5
dangerousCoder-OP <102338123+dangerousCoder-OP@users.noreply.github.com>
2026-01-13 16:07:37 +0530

    Delete Learning Python From Scratch/Labs directory

--- Learning Python From Scratch/Labs/Day1/Day1_5.py
+++
@@ -1,9 +0,0 @@
-numbers = []
-for i in range(1,21):
-    numbers.append(i)
-
-evenNumbers = filter(lambda x: x % 2 == 0, numbers)
-squaredEvens = map(lambda x: x * x, evenNumbers)
-
-for index, value in enumerate(squaredEvens):
-    print(index, value)
--- Learning Python From Scratch/Labs/Day1/Day1_7.py
+++
@@ -1,7 +0,0 @@
-data = [1, 2, 3, 4, 5, 6, 2, 4]
-
-uniqueEvens = set()
-for i in data:
-    if i % 2 == 0:
-        uniqueEvens.add(i)
-print(uniqueEvens)
